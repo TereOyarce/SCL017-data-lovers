5760d9bcc1690c6ccb982de7f0c3354e011d56aa
NatalyFarias <nataly.fariasg@gmail.com>
2021-06-10 11:32:25 -0400

    se modifica main.js

--- data.js
+++ data.js
@@ -1,5 +1,16 @@
 // estas funciones son de ejemplo
+import dataLOL from './data/lol/lol.js';
+//import data from './data/pokemon/pokemon.js';
+// import data from './data/rickandmorty/rickandmorty.js';
 
+const lista = document.getElementById('lista');
+// aqui transformo la dsata que viene como objeto en arreglo
+let dataLolAsArray = Object.values(dataLOL.data);
+dataLolAsArray.map(champion=>{
+    const li = document.createElement("li");
+    li.innerHTML = champion.info.difficulty
+    lista.appendChild(li)
+})
 export const example = () => {
   return 'example';
 };

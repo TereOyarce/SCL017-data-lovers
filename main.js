8fbaefad579b3741d068613a60e8c825af9b127b
Teresa Oyarce <t.oyarce05@gmail.com>
2021-06-10 11:13:49 -0400

    declarado main js

--- main.js
+++ main.js
@@ -1,6 +1,15 @@
-import { example } from './data.js';
-// import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
+import lol from './data/lol/lol.js';
+import data from './data/lol/lol.js';
+//import data from './data/pokemon/pokemon.js';
 // import data from './data/rickandmorty/rickandmorty.js';
+const abc = document.querySelector("abc");
+const tipos = document.querySelector("tipos");
+const stats = document.querySelector("stats");
+const dificultad = document.querySelector("dificultad");
 
-console.log(example, data);
+
+
+
+
+
+console.log(data);

39c22f368f5123b792ebf547381cdc429c0e6e78
NatalyFarias <nataly.fariasg@gmail.com>
2021-06-12 21:57:06 -0400

    se conectan los datos de los campeones a un contenedor en html

--- data.js
+++ data.js
@@ -3,13 +3,13 @@ import dataLOL from './data/lol/lol.js';
 //import data from './data/pokemon/pokemon.js';
 // import data from './data/rickandmorty/rickandmorty.js';
 
-const lista = document.getElementById('lista');
-// aqui transformo la dsata que viene como objeto en arreglo
+const contenedorCampeones = document.getElementById('contenedorCampeones');
+// aqui transformo la data que viene como objeto en arreglo
 let dataLolAsArray = Object.values(dataLOL.data);
-dataLolAsArray.map(champion=>{
-    const li = document.createElement("li");
-    li.innerHTML = champion.info.difficulty
-    lista.appendChild(li)
+//aqui agrego for each para recorrer la data
+dataLolAsArray.forEach(champion=>{
+  //aqui creo la card y la inserto en el contenedor padre
+  contenedorCampeones.innerHTML += '<div id="contenedor-' + champion.id + '"><img src="' + champion.img + '" width="100px"><p>' + champion.name + '</p><p style="text-align: justify;padding:15px;">'+ champion.blurb+'</p></div>';
 })
 export const example = () => {
   return 'example';
